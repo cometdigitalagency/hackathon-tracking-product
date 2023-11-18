6b8363321d39e8ff57b72e990d3fa7c3649fbed9
khamsyher <114057321+khamsyher@users.noreply.github.com>
2023-11-18 08:59:06 +0700

    first commit

---
+++ order_tracker/lib/order.dart
@@ -0,0 +1,11 @@
+class Order {
+  String orderId;
+  String productName;
+  int quantity;
+
+  Order({
+    required this.orderId,
+    required this.productName,
+    required this.quantity,
+  });
+}
