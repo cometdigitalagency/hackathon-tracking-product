329ca3f80aee9115b7c0b130fe01d92e9d830811
khamsyher <114057321+khamsyher@users.noreply.github.com>
2023-11-18 14:49:39 +0700

    commit code search

--- order_tracker/example/lib/main.dart
+++ order_tracker/example/lib/main.dart
@@ -1,82 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:order_tracker/order.dart';
 import 'package:order_tracker/order_tracker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  final OrderTracker orderTracker = OrderTracker();
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
+    return MaterialApp(
+      home: MyHomePage(
+        orderTracker: orderTracker,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  final OrderTracker orderTracker;
+
+  MyHomePage({required this.orderTracker, Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // Create a list of orders
-  List<Order> orders = [
-    Order(orderId: '1', productName: 'Product A', quantity: 3),
-    Order(orderId: '2', productName: 'Product B', quantity: 5),
-    Order(orderId: '3', productName: 'Product C', quantity: 2),
-  ];
-
-  // Use the Calculator to calculate the total quantity
-  Calculator calculator = Calculator();
-  late Future<int> totalQuantity;
+  late Future<void> futureData;
 
   @override
   void initState() {
     super.initState();
-    totalQuantity = calculator.calculateTotalQuantity(orders);
+
+    final String productId = 'DNXPL2706203943';
+
+    final Express expressType = Express.expressA;
+
+    futureData = widget.orderTracker.fetchData(
+      express: expressType,
+      productId: productId,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Order Tracker'),
+        title: const Text('Order Tracking Example'),
       ),
-      body: Column(
-        children: [
-          FutureBuilder<int>(
-            future: totalQuantity,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                int result = snapshot.data ?? 0;
-                return Text('Total Quantity: $result');
-              }
-            },
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Product: ${orders[index].productName}'),
-                  subtitle: Text('Quantity: ${orders[index].quantity}'),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+      body: ListView(),
     );
   }
 }
